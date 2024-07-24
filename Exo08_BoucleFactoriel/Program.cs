d6d63dae4d506d95976036c25604b6396a593edc
Samuel Legrain <samuel.legrain@bstorm.be>
2024-07-24 16:47:36 +0200

    Add Exo08 + Exo09

---
+++ Exo08_BoucleFactoriel/Program.cs
@@ -0,0 +1,13 @@
+﻿int nb;
+do
+{
+    Console.WriteLine("Veuillez introduire un nombre entier : ");
+} while (!int.TryParse(Console.ReadLine(), out nb) || nb > 20 || nb < 1);
+
+ulong result = 1;
+
+for (uint count = 2; count <= nb; count++)
+{
+    result = checked( result * count );
+}
+Console.WriteLine($"Le factoriel de {nb} est {result}!");
---
+++ Exo09_BoucleNombrePremier/Program.cs
@@ -0,0 +1,20 @@
+﻿int nb;
+do
+{
+    Console.WriteLine("Veuillez introduire le nombre de nombre premier à afficher : ");
+} while (!int.TryParse(Console.ReadLine(), out nb) || nb < 0);
+
+
+for (int count = 0, nbTeste = 2; count < nb; nbTeste++)
+{
+    bool moreDiv = false;
+    for (int diviseur = 2; diviseur <= (nbTeste/2) && !moreDiv; diviseur++)
+    {
+        if(nbTeste % diviseur == 0) moreDiv = true;
+    }
+    if(!moreDiv)
+    {
+        Console.WriteLine(nbTeste);
+        count++;
+    }
+}
